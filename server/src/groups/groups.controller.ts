0966c17e07bb457aec6ae3b1580cd79066e68788
NistonN <nisprogrammer@gmail.com>
2025-03-12 20:04:16 +0300

    groups

--- server/src/groups/groups.controller.ts
+++ server/src/groups/groups.controller.ts
@@ -1,7 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Schedule } from '@prisma/client';
+import { ChangeGroupsDto, Groups } from './dto/groups.types';
 import { GroupsService } from './groups.service';
 
 @Controller('groups')
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
+
+  @Post()
+  async add(
+    @Body('name') name: string,
+    @Query('api-key') apiKey: string,
+  ): Promise<Schedule> {
+    return await this.groupsService.add(name, apiKey);
+  }
+
+  @Get()
+  async get(@Query('api-key') apiKey: string): Promise<Groups[]> {
+    return await this.groupsService.get(apiKey);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put()
+  async change(
+    @Body() dto: ChangeGroupsDto,
+    @Query('api-key') apiKey: string,
+  ): Promise<Schedule> {
+    const { oldName, newName } = dto;
+    return await this.groupsService.change(oldName, newName, apiKey);
+  }
+
+  @Delete()
+  async delete(
+    @Body('name') name: string,
+    @Query('api-key') apiKey: string,
+  ): Promise<Schedule> {
+    return await this.groupsService.delete(name, apiKey);
+  }
 }
